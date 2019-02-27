db44a16f016bf0159b2f545ec87e19b4a21fbbd3
Bukunmi Aluko <oluwabukunmi.aluko@gmail.com>
2019-02-28 00:40:58 +0100

    Updates

---
+++ precache-manifest.99d53e3a141fdcae073a87b67814d450.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "24cefdeb1fc1b55338a2",
+    "url": "/contactsmanager/static/js/runtime~main.24cefdeb.js"
+  },
+  {
+    "revision": "c59b66afc27f5fb418ab",
+    "url": "/contactsmanager/static/js/main.c59b66af.chunk.js"
+  },
+  {
+    "revision": "6b50f60dcf4d2d55fb48",
+    "url": "/contactsmanager/static/js/2.6b50f60d.chunk.js"
+  },
+  {
+    "revision": "6b50f60dcf4d2d55fb48",
+    "url": "/contactsmanager/static/css/2.28fa4211.chunk.css"
+  },
+  {
+    "revision": "12bc84cc78444400cf4fd75cc5d7f9d3",
+    "url": "/contactsmanager/index.html"
+  }
+];
